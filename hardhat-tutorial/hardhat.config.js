aedf9b0fd0b086bd6e3ebf5301320e87e1597230
Muden <mudennn@yahoo.com>
2022-09-12 08:48:37 +0800

    change hardcode env

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -13,8 +13,8 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     rinkeby: {
-      url:"https://eth-rinkeby.alchemyapi.io/v2/ksSEYTnjInuKn_Wh-nwXraOAXQWKD1Tk",
-      accounts: ["bf2ee9b93e957e076d8d9280d7962126a33d9c040ab32ec8c1027015db4cee8e"],
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
     },
   },
 };
